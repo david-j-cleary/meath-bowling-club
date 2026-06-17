3f8c54d3d2fe0a4477932af50f44a5f62701e393
David Cleary <david.cleary.400@gmail.com>
2026-06-17 22:44:45 +0100

    Updated color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,17 @@ export default {
     extend: {
       colors: {
         club: {
-          50: '#F5F7F4',
-          100: '#E8EFE8',
-          300: '#A8B9A4',
-          500: '#4C6B4E',
-          700: '#334733'
+          50: '#F0F7F0',
+          100: '#D4EAD4',
+          200: '#A8D5A8',
+          300: '#7CC17C',
+          500: '#2D7A2D',
+          700: '#1B5E20',
+          900: '#0D3812'
+        },
+        accent: {
+          yellow: '#FFD700',
+          gold: '#FFC300'
         }
       }
     }
